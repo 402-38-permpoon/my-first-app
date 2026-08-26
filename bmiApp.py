5b2b194a088c1f27d791d0296979ab6a54188459
402-38-permpoon <54680@yupparaj.ac.th>
2026-08-26 15:34:55 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -9,19 +9,19 @@ height_cm = st.number_input("กรอกส่วนสูงของคุณ
 if st.button("คำนวณค่า BMI 🎯"):
     # แปลงส่วนสูงจาก cm เป็น เมตร แล้วคำนวณ BMI
     height_m = height_cm / 100
-    bmi = weight / height_m ** 2)
+    bmi = weight / (height_m ** 2)
 
     st.write("---")
     st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f**")
 
-  if bmi < 18.5:
+ if bmi < 18.5:
        st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
-  elif 18.5 <= bm9i < 23.0:
+ elif 18.5 <= bm9i < 23.0:
        st.warning("🎉 คุณมีน้ำหนักอยู๋เกณฑ์ปกติ (สุขภาพดี)")
-  elif 23.0 <= bmi < 25.0:
+ elif 23.0 <= bmi < 25.0:
        st.info("💡 คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
-  else:
+ else:
        st.error("🚨 คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
-
+                                   
 st.driver()
 st.write("นายเพิ่มพูล กันทวงค์ เลขที่ 38 ม.4/2")
